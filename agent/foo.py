8ed4bafb4e4a069952e7634cdf278e4ecdb6afb0
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-11-22 15:32:12 +0100

    websockets

--- agent/foo.py
+++ agent/foo.py
@@ -1,11 +1,7 @@
-import asyncio
-import subprocess
-
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-from fastapi import BackgroundTasks, FastAPI, WebSocket
 
 app = FastAPI()
-
 
 html = """
 <!DOCTYPE html>
@@ -14,15 +10,15 @@ html = """
         <title>Chat</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
+        <h1>WebSocket Chat 123</h1>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send FOoBVar</button>
+            <button>Send</button>
         </form>
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("http://localhost:8000/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -41,6 +37,7 @@ html = """
 </html>
 """
 
+
 @app.get("/")
 async def get():
     return HTMLResponse(html)
@@ -52,20 +49,3 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = await websocket.receive_text()
         await websocket.send_text(f"Message text was: {data}")
-
-
-async def run_deploy():
-    print("running deployment")
-    for i in range(10):
-        print("i: ", i)
-        await asyncio.sleep(1)
-    # print("deployment complete")
-    # cmd = subprocess.run("./deploy_cast_hosting.sh", capture_output=True)
-    # print("stdout: ", cmd.stdout.decode("utf8"))
-
-
-@app.post("/deploy")
-async def deploy(background_tasks: BackgroundTasks):
-    print("received deploy event")
-    background_tasks.add_task(run_deploy)
-    return {"message": "deploying"}
